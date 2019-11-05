2525697c6910c01945cb0f432897595d84d7961e
user.name <user.name@email.com>
2019-11-05 15:59:31 +0000

    Fleshed out the agent class slightly more. Added some more tests

--- agent.py
+++ agent.py
@@ -3,18 +3,26 @@ Module that contains the Agent class and relevant subclasses for the zombie outb
 """
 
 class Agent:
-    def __init__(self, name, position):
-        self.name = name
+    def __init__(self, name, position, speed=0.5):
+        self.__name = name
         self.position = position
+        self.speed = speed
+    
+    __agentType = 'a'
 
     @property
     def name(self):
-        return self._name
+        return self.__name
 
     @name.setter
     def name(self, value):
-        self._name = value
-
+        if 0 <= value < 100000:
+            self.__name = value
+        elif value < 0 or value > 100000:
+            raise ValueError("The id value must be of type int and in the range [0, 100000]")
+        elif type(value) != int:
+            raise TypeError("The id value must be of type int and in the range [0, 100000)")
+ 
     @property
     def position(self):
         return self._position
@@ -23,12 +31,45 @@ class Agent:
     def position(self, value):
         if (type(value) is list or type(value) is tuple) and len(value) == 2:
             self._position = value
-        elif (type(value) is not list or type(value) is not tuple):
-            raise ValueError("The position must be given as a list or tuple")
-        elif len(value) == 2:
+        elif (type(value) is not list and type(value) is not tuple):
+            raise TypeError("The position must be given as a list or tuple")
+        elif len(value) != 2:
             raise ValueError("The position must be 2-dimensional")
           
+    @property
+    def speed(self):
+        return self._speed
 
+    @speed.setter
+    def speed(self, value):
+        if 0.0 < value <= 1.0:
+            self._speed = value
+        elif value <= 0.0 or speed > 1.0 :
+            raise ValueError("Speed must be in the range (0, 1]")
+        elif type(value) != int:
+            raise TypeError("Speed must be either an integer or a float")
+
+    
+    def __generate_name(self, value):
+        print(value)
+        if value < 10:
+            return self.__agentType + "_0000" + str(value)
+        elif 10 <= value < 100:
+            return self.__agentType + "_000" + str(value)
+        elif 100 <= value < 1000:
+            return self.__agentType + "_00" + str(value)
+        elif 1000 <= value < 10000:
+            return self.__agentType + "_0" + str(value)
+        elif 10000 <= value < 100000:
+            return self.__agentType + "_" + str(value)
+        elif value < 0 or value > 100000:
+            raise ValueError("The id value must be of type int and in the range [0, 100000]")
+        elif type(value) != int:
+            raise TypeError("The id value must be of type int and in the range [0, 100000)")
+        
+    def move_position(self, neighbour_position):
+        self._position = tuple([sum(x) for x in zip(self._position, neighbour_position)])
+        
     def move_north(self):
         self._position = tuple([sum(x) for x in zip(self._position, (-1, 0))])
     
@@ -55,10 +96,20 @@ class Agent:
 
 
 class Human(Agent):
-    def __init__(self, name, position):
-        Agent.__init__(self, name, position)
+    def __init__(self, obj_id, position, speed=0.7):
+        Agent.__init__(self, obj_id, position, speed)
+        self.agent_type = 'h'
 
 
 class Zombie(Agent):
-    def __init__(self, name, position):
-        Agent.__init__(self, name, position)
+    def __init__(self, obj_id, position, speed=0.3):
+        Agent.__init__(self, obj_id, position, speed)
+        self.agent_type = 'z'
+
+
+# new = Agent(1,(2,3))
+# print(new.name)
+# print(new.position)
+# print(new.speed)
+# new.name = 2
+# print(new.name)
--- test_agent.py
+++ test_agent.py
@@ -12,6 +12,14 @@ class TestAgent(unittest.TestCase):
         self.assertEqual(a.name, 2)        
 
 
+    def test_name_not_correct_type(self):
+        a = agent.Agent(1, (3, 3))
+        with self.assertRaises(TypeError):
+            a.name = '4'
+        with self.assertRaises(ValueError):
+            a.name = -1
+
+
     def test_position(self):
         a = agent.Agent(1, (3, 3))
         self.assertEqual(a.position, (3, 3))
@@ -22,11 +30,33 @@ class TestAgent(unittest.TestCase):
     
     def test_position_not_correct_type(self):
         a = agent.Agent(1, (3, 3))
-        with self.assertRaises(ValueError):
+        with self.assertRaises(TypeError):
             a.position = 4
         with self.assertRaises(ValueError):
-            a.position = '4'
-  
+            a.position = (1,2,3)
+
+
+    def test_speed(self):
+        a = agent.Agent(1, (3, 3))
+        self.assertEqual(a.speed, 0.5)
+        
+        a.speed = 0.9
+        self.assertEqual(a.speed, 0.9)    
+
+    
+    def test_speed_not_correct_type(self):
+        a = agent.Agent(1, (3, 3))
+        with self.assertRaises(ValueError):
+            a.speed = 0
+        with self.assertRaises(TypeError):
+            a.speed = '0.6'
+            
+
+    def test_move_position(self):
+        a = agent.Agent(1, (0, 0))
+        a.move_position((1,1))
+        self.assertEqual(a.position, (1, 1))
+
 
     def test_move_north(self):
         a = agent.Agent(1, (0, 0))
@@ -80,12 +110,13 @@ class TestAgent(unittest.TestCase):
         h = agent.Human(1, (0, 0))
         self.assertEqual(h.name, 1)
         self.assertEqual(h.position, (0, 0))
+        self.assertEqual(h.speed, 0.7)
 
 
     def test_zombie(self):
         z = agent.Zombie(1, (0, 0))
         self.assertEqual(z.name, 1)
-        self.assertEqual(z.position, (0, 0))
+        self.assertEqual(z.speed, 0.3)
 
 
 if __name__ == "__main__":
